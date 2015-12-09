05c7309a7bd509416b6517def88b1ce247cce320
James Traver <jtraverjr@gmail.com>
2015-12-09 10:21:35 -0600

    routes defined

--- app.rb
+++ app.rb
@@ -5,3 +5,60 @@ ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :database => 'security_pract_dev'
 )
+
+enable :sessions
+
+
+# define a few session helps
+def get_current_user_if_exists
+  if session[:user]
+    return session[:user]
+  else
+    return nil
+  end
+end
+
+def log_current_user_out
+  if get_current_user_if_exists = nil
+    return false
+  else
+    session[:user] = nil
+    return true
+  end
+end
+
+# routes
+get '/' do
+  # some content
+  # binding.pry
+  erb :hot_stuff
+end
+
+get '/logout' do
+  # clear our session
+  # redirect home
+  log_current_user_out
+  redirect '/'
+end
+
+get '/foyer' do
+  # present user with a form to login
+  # or a form to register
+  erb :foyer
+end
+
+post '/login' do
+  # check params
+  # validate password
+  # log user in
+  # redirect home
+  # add message to session to welcome user
+end
+
+post '/register' do
+  # check params
+  # validate password
+  # log user in
+  # redirect home
+  # add message to session to thank user for registering
+end
